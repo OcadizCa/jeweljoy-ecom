712d42b6b9885331dceb267bce5e7e71223dca41
Cath <ocadiz.ca@gmail.com>
2024-07-11 20:22:36 -0600

    Se hicieron cambios en html y css

--- store/urls.py
+++ store/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('product/new/', views.product_create, name='product_create'),
     path('product/<int:pk>/edit/', views.product_update, name='product_update'),
     path('product/<int:pk>/delete/', views.product_delete, name='product_delete'),
+    path('home/',views.home, name="Home")
 ]
